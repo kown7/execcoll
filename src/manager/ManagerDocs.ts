5cb968f96e4e70f769a78d5c44f2a4b049abb9a7
Kristoffer Nordstroem <kristoffer.nordstroem@gmail.com>
2023-11-26 14:39:57 +0100

    Happy lsp-mode linter

--- src/manager/ManagerDocs.ts
+++ src/manager/ManagerDocs.ts
@@ -1,52 +1,50 @@
 import json from "../assets/data.json"
-import { createPdf } from "../access/AccessPDF.ts"
+import { createPdf } from "../access/AccessPDF"
 import demostring from "./demo.txt?raw"
 
-export interface ManagerDocs {
-  docCfgTemplate: {
-    uuid: string;
-    fields: Map<string, string>;
-  }
-
-  docCfgItem: {
-    uuid: string;
-    subElements: Array<string>;  // string of uuid
-  }
-
-  docCfg: {
-    itemSelection: Array<docCfgItem>;
-    template: docCfgTemplate;
-  }
-
-  ExerciseItem: {
-    uuid: string;
-    author: string;
-    title: string;
-    headers: Array<string>;  // string of uuid
-    license?: string
-    content: string
-  }
-
-  RetDoc: {
-    document: any;
-    exception?: any;
-  }
-
-  FilterExercises: {
-    author?: string;
-  }
+export interface IDocCfgTemplate {
+	uuid: string;
+	fields: Map<string, string>;
 }
 
-export function filterExercises(filter?: FilterExercises): Array<ExerciseItem> {
-  // expose managed state as return value
-  return json['exercises']
+export interface IDocCfgItem {
+	uuid: string;
+	subElements: Array<string>;  // string of uuid
 }
 
-export function filterTemplates(filter?: FilterTemplates): Array<TemplateItem> {
-  // expose managed state as return value
-  return json['templates']
+export interface IDocCfg {
+	itemSelection: Array<IDocCfgItem>;
+	template: IDocCfgTemplate;
 }
 
-export async function generateDocument(cfg: docCfg) {
-  return createPdf(demostring);
+export interface ExerciseItem {
+	uuid: string;
+	author: string;
+	title: string;
+	headers?: Array<string>;  // string of uuid
+	license?: string
+	content: string
+}
+
+export interface IRetDoc {
+	document: any;
+	exception?: any;
+}
+
+export interface IFilterExercises {
+	author?: string;
+}
+
+export function filterExercises(filter?: IFilterExercises): Array<ExerciseItem> {
+	// expose managed state as return value
+	return json['exercises']
+}
+
+//export function filterTemplates(filter?: FilterTemplates): Array<TemplateItem> {
+//	// expose managed state as return value
+//	return json['templates']
+//}
+
+export async function generateDocument(cfg: IDocCfg) {
+	return createPdf(demostring);
 }
