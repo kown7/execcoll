295096bc7b784ee27ae0bd19fa1bce497fa2cebd
Kristoffer Nordstroem <kristoffer.nordstroem@gmail.com>
2023-12-01 00:12:06 +0100

    Format

--- src/access/AccessItems.ts
+++ src/access/AccessItems.ts
@@ -1,107 +1,109 @@
-import json from "../assets/data.json"
+import json from '../assets/data.json'
 
 export enum FilterItemsType {
   All = 0,
   Exercise = 1,
   Template,
-  Header,
+  Header
 }
 
 export interface IFilterItems {
-  itemType?: FilterItemsType;
-  uuid?: string;
-  author?: string;
-  searchString?: string;
+  itemType?: FilterItemsType
+  uuid?: string
+  author?: string
+  searchString?: string
 }
 
 export interface IResultItem {
-  uuid: string;
-  author?: string;
-  title?: string;
-  headers?: Array<string>;  // string of uuid
+  uuid: string
+  author?: string
+  title?: string
+  headers?: Array<string> // string of uuid
   license?: string
   content: string
 }
 
 class ExerciseItem implements IResultItem {
-  uuid: string;
-  author: string;
-  title: string;
-  headers?: Array<string>;  // string of uuid
-  license?: string;
-  content: string;
+  uuid: string
+  author: string
+  title: string
+  headers?: Array<string> // string of uuid
+  license?: string
+  content: string
 }
 
 class HeaderItem implements IResultItem {
-  uuid: string;
-  author: string;
-  content: string;
+  uuid: string
+  author: string
+  content: string
 }
 
 class TemplateItem implements IResultItem {
-  uuid: string;
-  title: string;
-  author?: string;
-  fields?: Array<string>;  // string of fields
-  license?: string;
-  content: string;
+  uuid: string
+  title: string
+  author?: string
+  fields?: Array<string> // string of fields
+  license?: string
+  content: string
 }
-
 
 export function filterExerciseItems(filter: Array<IFilterItems>): Array<ExerciseItem> {
-  var result: ExerciseItem[] = [];
+  var result: ExerciseItem[] = []
   for (var filterItem of filter) {
     for (var item of json['exercises']) {
-      if (((!filterItem.uuid) || (item.uuid == filterItem.uuid)) &&
-	((!filterItem.author) || (item.author == filterItem.author))) {
-	  var r = new ExerciseItem();
-	  r.uuid = item.uuid;
-	  r.author = item.author;
-	  r.title = item.title;
-	  r.headers = item.headers;
-	  r.license = item.license;
-	  r.content = item.content;
-	  result.push(r);
-	}
+      if (
+        (!filterItem.uuid || item.uuid == filterItem.uuid) &&
+        (!filterItem.author || item.author == filterItem.author)
+      ) {
+        var r = new ExerciseItem()
+        r.uuid = item.uuid
+        r.author = item.author
+        r.title = item.title
+        r.headers = item.headers
+        r.license = item.license
+        r.content = item.content
+        result.push(r)
+      }
     }
   }
-  return result;
+  return result
 }
 
-
 export function filterHeadersItems(filter: Array<IFilterItems>): Array<HeaderItem> {
-  var result: HeaderItem[] = [];
+  var result: HeaderItem[] = []
   for (var filterItem of filter) {
     for (var item of json['headers']) {
-      if (((!filterItem.uuid) || (item.uuid == filterItem.uuid)) &&
-	((!filterItem.author) || (item.author == filterItem.author))) {
-	  var r = new HeaderItem();
-	  r.uuid = item.uuid;
-	  r.author = item.author;
-	  r.content = item.content;
-	  result.push(r);
-	}
+      if (
+        (!filterItem.uuid || item.uuid == filterItem.uuid) &&
+        (!filterItem.author || item.author == filterItem.author)
+      ) {
+        var r = new HeaderItem()
+        r.uuid = item.uuid
+        r.author = item.author
+        r.content = item.content
+        result.push(r)
+      }
     }
   }
-  return result;
+  return result
 }
 
 export function filterTemplateItems(filter: Array<IFilterItems>): Array<TemplateItem> {
   // expose managed state as return value
-  var result: TemplateItem[] = [];
+  var result: TemplateItem[] = []
   for (var filterItem of filter) {
     for (var item of json['templates']) {
-      if (((!filterItem.uuid) || (item.uuid == filterItem.uuid))) {
-	let r = new TemplateItem();
-	r.uuid = item.uuid;
-	r.title = item.title;
-	r.author = item.author;
-	r.fields = item.fields;
-	r.license = item.license;
-	r.content = item.content;
-	result.push(r);
+      if (!filterItem.uuid || item.uuid == filterItem.uuid) {
+        let r = new TemplateItem()
+        r.uuid = item.uuid
+        r.title = item.title
+        r.author = item.author
+        r.fields = item.fields
+        r.license = item.license
+        r.content = item.content
+        result.push(r)
       }
     }
   }
-  return result;
+  return result
 }
