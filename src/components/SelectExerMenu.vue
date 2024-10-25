cd28837d90c260ef09d952acb4e1b5b873ebacce
Kristoffer Nordstroem <kristoffer.nordstroem@gmail.com>
2024-10-25 15:42:38 +0200

    Select elements with double click

--- src/components/SelectExerMenu.vue
+++ src/components/SelectExerMenu.vue
@@ -17,7 +17,10 @@
           <h2 class="text-lg font-bold">Available Items</h2>
           <draggable class="list-group" :list="list2" group="people" itemKey="name">
             <template #item="{ element, index }">
-              <div class="p-2 bg-white border mb-2 cursor-move">
+              <div
+                class="p-2 bg-white border mb-2 cursor-move"
+                @dblclick="switchElement(`${element.uuid}`, this.list2, this.list1)"
+              >
                 {{ element.title }} <br />
                 <img :src="`previews/${element.preview}`" />
               </div>
@@ -31,7 +34,10 @@
           <h2 class="text-lg font-bold">Selected Items</h2>
           <draggable class="list-group" :list="list1" group="people" @change="log" itemKey="name">
             <template #item="{ element }">
-              <div class="p-2 bg-white border mb-2 cursor-move">
+              <div
+                class="p-2 bg-white border mb-2 cursor-move"
+                @dblclick="switchElement(`${element.uuid}`, this.list1, this.list2)"
+              >
                 <img :src="`previews/${element.preview}`" />
               </div>
             </template>
@@ -86,6 +92,14 @@ export default {
     },
     log: function (evt) {
       this.mainStore.set(this.list1)
+    },
+    switchElement: function (uuid, src, dest) {
+      console.log('Switch: ', uuid)
+      let destTmp = src.filter((item) => item.uuid == uuid)
+      let tmp = src.filter((item) => item.uuid != uuid)
+      src.length = 0
+      src.push(...tmp)
+      dest.push(...destTmp)
     }
   }
 }
